c79b1d2f753449eaf90affd133636396e5654df1
S.M. Mahmudul Hasan Rudra <71984328+Mahmudul-Rudra@users.noreply.github.com>
2024-07-09 14:39:50 +0600

    Header and Footer added

---
+++ client/src/Components/Layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h4 className="text-center">
+        All Rights Reserved &copy; S. M. Mahmudul Hasan Rudra
+      </h4>
+    </div>
+  );
+};
+
+export default Footer;
